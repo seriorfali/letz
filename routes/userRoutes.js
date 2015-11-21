447c710b55ad508c5933c19c97e99dca144daa5b
Sean Esteva <seangeleno@gmail.com>
2015-11-21 08:22:51 -0800

    added userRouter .get and .post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,17 @@ var express      = require('express')
 ,   passport     = require('passport')
 ,   userRouter   = express.Router();
 
+
+//first we have the login route with a .get and .post method attached to it
 userRouter.route('/login')
   .get(function(req, res){
     res.render('login', {message: req.flash('loginMessage')});
   })
+
+//and now .post
+
+  .post(passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
