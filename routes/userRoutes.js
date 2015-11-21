5aca532313aceb7faf1cd4e18e3d0642d46cb398
Sean Esteva <seangeleno@gmail.com>
2015-11-21 09:51:24 -0800

    added facebook routes and basic module referencing at the top

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,27 +5,49 @@ var express      = require('express')
 
 
 //first we have the login route with a .get and .post method attached to it
-userRouter.route('/login')
-  .get(function(req, res){
-    res.render('login', {message: req.flash('loginMessage')});
+userRouter.route("/login")
+  .get(function(req, res) {
+    res.render("login", {message: req.flash("loginMessage")});
   })
-
-//and now .post
-
-  .post(passport.authenticate('local-login', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
+  .post(passport.authenticate("local-login", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+    // To use flash messages specified for errors.
     failureFlash: true
   }))
 
-//secondly we have the userRouter for signing up
-userRouter.route('/signup')
-  .get(function(req, res){
-    res.render('signup', {message: req.flash('signupMessage')})
+userRouter.route("/signup")
+  .get(function(req, res) {
+    res.render("signup", {message: req.flash("signupMessage")});
   })
-  // do .post again
-  .post(passport.authenticate('local-signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
+  .post(passport.authenticate("local-signup", {
+    successRedirect: "/profile",
+    failureRedirect: "/signup",
+    // To use flash messages specified for errors.
     failureFlash: true
   }))
+
+userRouter.get("/profile", isLoggedIn, function(req, res) {
+  res.render("profile", {user: req.user})
+})
+
+// Facebook routes.
+userRouter.get("/auth/facebook", passport.authenticate("facebook", {scope: ["email"]}))
+
+userRouter.get("/auth/facebook/callback", passport.authenticate("facebook", {
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}))
+
+userRouter.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+})
+
+// To check if user is authenticated.
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect("/");
+}
+
+module.exports = userRouter;
