c1eca300ae1900ae51f270f1a091b0043cee98b4
Santiago <santiago@nowhereprod.com>
2015-11-21 12:11:00 -0800

    Added passport authentication in Passport.js file and auth.js for facebook authentication.

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'facebookAuth': {
+      'clientID': '1026928810712683',
+      'clientSecret': '77b8359aa87832d99564a3e7f5e8fd6d',
+      'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+      'profileFields': ['emails', 'displayName']
+  }
+}
