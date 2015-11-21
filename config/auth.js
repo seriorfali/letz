b4fd6ac03603299079f95c11a0243641f9acd206
Santiago <santiago@nowhereprod.com>
2015-11-21 12:19:13 -0800

    update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'facebookAuth': {
-      'clientID': '1026928810712683',
-      'clientSecret': '77b8359aa87832d99564a3e7f5e8fd6d',
+      'clientID': '1729339443964133',
+      'clientSecret': 'c1b8d8bdc1b79b47869b2509a45029ff',
       'callbackURL': 'http://localhost:3000/auth/facebook/callback',
       'profileFields': ['emails', 'displayName']
   }
