031e1144739ca58af77fdf7863c4202becddc58a
Sean Esteva <seangeleno@gmail.com>
2015-11-21 07:58:26 -0800

    added show function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,13 +10,15 @@ var User = require('../models/User.js')
 
 //need to create action to create a new user
   function show(req, res) {
-
+    User.find({email: req.params.email},function(err,user){
+      if(err) throw err
+      res.json(user)
     })
   }
 
 //need to create action to show a single user
   function create(req, res){
-
+    
     }
 
   function update(req, res){
@@ -29,7 +31,7 @@ var User = require('../models/User.js')
 
 module.exports = {
   indexUser: index,
-  createUser: create,
+  addUser: create,
   showUser: show,
   updateUser: update,
   destroyUser: destroy
