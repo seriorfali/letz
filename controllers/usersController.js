4617e302b3177ee4a18029903ce380ac18c5e1c0
Sean Esteva <seangeleno@gmail.com>
2015-11-21 07:48:38 -0800

    added connection to model and basic set-up for the controllers including the module.exports

---
+++ controllers/usersController.js
@@ -0,0 +1,34 @@
+var User = require('../models/User.js')
+
+//index shows all the users
+  function index(req, res) {
+    
+    })
+  }
+
+//need to create action to create a new user
+  function show(req, res) {
+
+    })
+  }
+
+//need to create action to show a single user
+  function create(req, res){
+
+    }
+
+  function update(req, res){
+
+    }
+
+  function destroy(req,res){
+
+    }
+
+module.exports = {
+  indexUser: index,
+  createUser: create,
+  showUser: show,
+  updateUser: update,
+  destroyUser: destroy
+}
