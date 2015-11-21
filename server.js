f56f824eb4a6ac500864f521debb42606f170fb1
Sean Esteva <seangeleno@gmail.com>
2015-11-21 10:53:47 -0800

    added index.html for the chat with sockets.io

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ webSocketsProvider.on("connection", function(socket) {
 })
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html')
 });
 
 
