aedaddedd08cd2c7ee73b66e16eec9874eeb2bf8
Sean Esteva <seangeleno@gmail.com>
2015-11-23 12:08:31 -0800

    trying to integrate chat

--- server.js
+++ server.js
@@ -8,39 +8,63 @@ var express = require("express")
   , session = require("express-session")
   , passport = require("passport")
   , passportConfig = require("./config/passport.js")
-  , http = require("http")
+  , http = require("http")/*.Server(app)*/
     // To allow HTTP to be bound to same port as WebSockets.
   , httpServer = http.Server(app)
     // To have provider of WebSockets connection to client listen at same port as HTTP.
-  , webSocketsProvider = require(socket.io)(httpServer)
+  // , webSocketsProvider = require("socket.io")(httpServer)
+  , io   = require('socket.io')(httpServer)
+  , connectedUsers = {}
+
+  // Database connection.
+  // mongoose.connect("mongodb://seriorfali:oolpI700#@ds045054.mongolab.com:45054/letz-app")
+  mongoose.connect("mongodb://localhost/myapp")
 
 // Middleware.
 app.use(logger("dev"))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.use(session({
+	secret: process.env.LETZ_SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use("/public", express.static(__dirname + "/public"))
 
-// Database connection.
 
-
-// User routes.
+// API routes.
 var userRoutes = require("./routes/userRoutes.js")
-app.use("/", userRoutes)
+app.use("/api/users", userRoutes)
+
+// Frontend routes.
+app.get("*", function(req, res) {
+  // res.render("index", {currentUser: req.user})
+  res.sendFile(__dirname + "/views/index.html")
+})
 
 // WebSocket callbacks.
-webSocketsProvider.on("connection", function(socket) {
+io.on("connection", function(socket) {
   console.log("A user connected.")
 })
 
+//web socket chat
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
+  res.sendfile('/views/chat.html');
 });
 
+//sockets chat connection
+io.on('connection', function(socket){
+  console.log('a user connected')
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 // Environment port.
-var port = process.env.PORT || 1000
+var port = process.env.PORT || 3000
 
 httpServer.listen(port, function() {
   console.log("Server running on port " + port + ".")
