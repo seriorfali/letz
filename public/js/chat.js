fcb85167ff26ac6d4743624f8a131d644e127854
Seri <seriorfali@gmail.com>
2015-11-28 14:49:13 -0800

    Added functions to send and accept chat requests, and functionality to generate private chat rooms and update them with member messages.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,30 +1,50 @@
-$(function() {
-  function generateChat() {
-    // Connection to socket.io.
-    var socket = io()
-    var currentUser
+function receiveChatRequests() {
+  socket.on("chat request", function(data) {
+    var chatRequest = "<div class='chatRequests'>" + "<p>" + ((data.requestingUser.local.first_name + " " + data.requestingUser.local.last_name) || data.requestingUser.facebook.name) + " would like to chat." + "</p>" + "<button class='acceptChatRequest' type='button'>ACCEPT</button>" + "</div>"
 
-    // AJAX request to retrieve current user.
-    $.get("/api/users/current")
-    .done(function(user) {
-      currentUser = user
-      console.log(currentUser)
+    $("#container").append(chatRequest)
+
+    $(".acceptChatRequest").click(function() {
+      socket.emit("accepted request", data)
+      generateChat(data)
     })
+  })
+}
 
-    $(".chats").dialog()
+function sendChatRequest() {
+  var targetUser = userMarker.user
 
-    // To send to server a message containing user chat message and chat window ID when send button clicked.
-    $(".sendMessage").submit(function() {
-      var message = {
-        body: $(this).children(".newMessages").val(),
-        chatWindowId: $(this).closest().attr("id")
-      }
-      socket.emit("chat message", message)
-    })
+  var sentRequest = "Chat request sent to " + ((targetUser.local.first_name + " " + targetUser.local.last_name) || targetUser.facebook.name) + "."
 
-    socket.on("update chat", function(data) {
-      $(".chats[id='" + data.chatWindowId + "'] .messages").append($("<li>".text("<b>" + (currentUser.local.first_name + " " + currentUser.local.last_name || currentUser.facebook.name) + " " + "</b>" + data.body + "<br>")))
-      console.log(data.body)
-    })
-  }
-})
+  infoWindow.content = sentRequest
+
+  socket.emit("chat request", {targetUser: targetUser, requestingUser: currentUser})
+
+  socket.on("accepted request", function(data) {
+    infoWindow.close()
+    generateChat()
+  })
+}
+
+function generateChat(users) {
+  var chatId = users.requestingUser.socketId + users.targetUser.socketId
+
+  var chatWindow = "<div class='chats' id='" + chatId + "'>" + "<ul class='messages'></ul>" + "<form class='sendMessage' action=''>" + "<input class='newMessages' autocomplete='off' /><button>Send</button>" + "</form>" + "</div>"
+
+  $("#container").append(chatWindow)
+
+  $("#" + chatId).dialog()
+
+  // To send to server a message containing user chat message and chat window ID when send button clicked.
+  $(".sendMessage").submit(function() {
+    var message = {
+      body: $(this).children(".newMessages").val(),
+      chatId: chatId
+    }
+    socket.emit("chat message", message)
+  })
+
+  socket.on("update chat", function(data) {
+    $(".chats[id='" + data.chatId + "'] .messages").append($("<li>"+ "<b>" + ((currentUser.local.first_name + " " + currentUser.local.last_name) || currentUser.facebook.name) + "</b>" + " " + data.body + "<br>")))
+  })
+}
