68cb420a7eb3f64f12084cb7bc0cdc0e1a4ed3c0
Seri <seriorfali@gmail.com>
2015-11-28 16:05:18 -0800

    Refined chat functionality.

--- public/js/chat.js
+++ public/js/chat.js
@@ -24,7 +24,7 @@ function sendChatRequest(userMarker, infoWindow) {
 
   socket.on("accepted request", function(data) {
     infoWindow.close()
-    generateChat()
+    generateChat(data)
   })
 }
 
