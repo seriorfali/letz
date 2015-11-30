004d5971fd813d561996d36c97f8270d03440416
Seri <seriorfali@gmail.com>
2015-11-29 17:34:45 -0800

    Fixed merge conflict, added map.css file.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-$(function() {
-  function generateChat() {
-    // Connection to socket.io.
-    var socket = io()
-    var currentUser
-
-    // AJAX request to retrieve current user.
-    $.get("/api/users/current")
-    .done(function(user) {
-      currentUser = user
-      console.log(currentUser)
-    })
-
-    $(".chats").dialog()
-
-    // To send to server a message containing user chat message and chat window ID when send button clicked.
-    $(".sendMessage").submit(function() {
-      var message = {
-        body: $(this).children(".newMessages").val(),
-        chatWindowId: $(this).closest().attr("id")
-      }
-      socket.emit("chat message", message)
-    })
-
-    socket.on("update chat", function(data) {
-      $(".chats[id='" + data.chatWindowId + "'] .messages").append($("<li>".text("<b>" + (currentUser.local.first_name + " " + currentUser.local.last_name || currentUser.facebook.name) + " " + "</b>" + data.body + "<br>")))
-      console.log(data.body)
-    })
-  }
-})
-=======
 var chats = []
 
 function getChat(chatId) {
@@ -161,4 +129,3 @@ function generateChat(data) {
     $(".chats[id='" + data.chatId + "'] .messages").append("<li>"+ "<b>" + getName(user) + "</b>" + " has left the chat.")
   })
 }
->>>>>>> d4ff99cd272bca2dd49ec0660dbbfaae7e9408a9
